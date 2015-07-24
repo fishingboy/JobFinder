661f9edb4735c62757d401785a13b832928cd733
jonic <jonic@uitox.com>
2015-07-24 16:39:47 +0800

    add pagenavi

--- resources/views/lists/job.blade.php
+++ resources/views/lists/job.blade.php
@@ -1,24 +1,1 @@
-@extends('lists/main')
-
-@section('content')
-<section class="content">
-	<table class="jobtable">
-		<thead>
-			<tr>
-				<!-- <th><input type="checkbox" class="checkbox-orders" name="checkbox-orders"></th> -->
-				<!-- <th>職缺列表</th> -->
-				<th>刊登日期</th>
-				<th>職稱</th>
-				<th>公司名稱<button class="table-sort"><i class="fa fa-chevron-down"></i></button></th>
-				<th>新資範圍<button class="table-sort"><i class="fa fa-chevron-up"></i></button></th>
-				<th>所在地</th>
-				<th>總人數</th>
-				<th>經歷需求</th>
-				<th>職缺數</th>
-				<th>資本額</th>
-			</tr>
-		</thead>
-	<tbody id="jobListBody">
-	</tbody>
-	</table>
-</section><script id="jobListTmpl" type="x-tmpl-mustache">		@{{#rows}}		<tr>			<td>@{{created_at}}</td>			<td><div class="fixword">@{{title}}</td>			<td><div class="fixword">@{{name}}</div></td>			<td>@{{sal_month_low}} ~ @{{sal_month_high}}</td>			<td>@{{job_addr_no_descript}}</td>			<td>@{{employees}}</td>			<td>@{{period}}</td>			<td>@{{job_count}}</td>			<td>@{{capital}}</td>		</tr>		@{{/rows}}</script>@stop@section('customJs')	<script src="{{ URL::asset('/js/lists/job.js') }}" charset="utf-8"></script>@stop
+@extends('lists/main')@section('content')<section class="content">	<table class="jobtable">		<thead>			<tr>				<!-- <th><input type="checkbox" class="checkbox-orders" name="checkbox-orders"></th> -->				<!-- <th>職缺列表</th> -->				<th>刊登日期</th>				<th>職稱</th>				<th>公司名稱<button class="table-sort"><i class="fa fa-chevron-down"></i></button></th>				<th>新資範圍<button class="table-sort"><i class="fa fa-chevron-up"></i></button></th>				<th>所在地</th>				<th>總人數</th>				<th>經歷需求</th>				<th>職缺數</th>				<th>資本額</th>			</tr>		</thead>	<tbody id="jobListBody">	</tbody>	</table>	<div class="pagenavi">		<ul>			<li class="prev"><a href="#"><i class="fa fa-chevron-left"></i></a></li>			<li class="active"><a href="#" >1</a></li><!-- 作用中 -->			<li><a href="#">2</a></li>			<li><a href="#">3</a></li>			<li><a href="#">4</a></li>			<li><a href="#">5</a></li>			<li class="next"><a href="#"><i class="fa fa-chevron-right"></a></i></li>					</ul>	</div></section><script id="jobListTmpl" type="x-tmpl-mustache">		@{{#rows}}		<tr>			<td>@{{created_at}}</td>			<td><div class="fixword">@{{title}}</td>			<td><div class="fixword">@{{name}}</div></td>			<td>@{{sal_month_low}} ~ @{{sal_month_high}}</td>			<td>@{{job_addr_no_descript}}</td>			<td>@{{employees}}</td>			<td>@{{period}}</td>			<td>@{{job_count}}</td>			<td>@{{capital}}</td>		</tr>		@{{/rows}}</script>@stop@section('customJs')	<script src="{{ URL::asset('/js/lists/job.js') }}" charset="utf-8"></script>@stop
