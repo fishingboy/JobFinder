8aaf583b0f8298ef3bbe01b9831671c61e7675e7
RAIN <rainlay@gmail.com>
2015-08-07 15:07:20 +0800

    增加 jgmap.js , 增加 捷運選單

--- resources/views/gmap/gmap.blade.php
+++ resources/views/gmap/gmap.blade.php
@@ -8,14 +8,29 @@
 	</style>
 </head>
 <body>
+	<div id="mrtSelector">
 
+	</div>
 	<div id="map-canvas"></div>
 
+	<script id="mrt-template" type="text/x-handlebars-template">
+		<select>
+		@{{#each .}}
+			 <option value="@{{latitude}},@{{longitude}}">@{{name}}</option>
+		@{{/each}}
+		</select>
+	</script>
+
+	<script src="{{ asset("js/3rd-party/jquery-2.1.4.min.js") }}" charset="utf-8"></script>
+	<script src="{{ asset("js/3rd-party/handlebars-v3.0.3.js") }}" charset="utf-8"></script>
 	<script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDwibvTIKkJFEfmx8gQuwZgGsPridiX08I"> </script>
 	<script src="{{ asset("js/jgmap.js") }}" charset="utf-8"></script>
+
+
 	<script charset="utf-8">
 		var jgmap = JGMAP;
-		jgmap.init();
+		// jgmap.init();
+		jgmap.buildTaipeiMrtSelector();
 	</script>
 </body>
 </html>
