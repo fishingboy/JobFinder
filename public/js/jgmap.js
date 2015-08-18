accd1385bc67d6976b662bde2b5bf1a09c04da93
RAIN <rainlay@gmail.com>
2015-08-18 16:49:26 +0800

    restore arrow

--- public/js/jgmap.js
+++ public/js/jgmap.js
@@ -99,12 +99,6 @@ var JGMAP = (function(google, $) {
 					iwBackground.children(':nth-child(4)').css({
 						'display': 'none'
 					});
-					iwBackground.children(':nth-child(1)').css({
-						'top': '304px'
-					});
-					iwBackground.children(':nth-child(3)').css({
-						'top': '304px'
-					});
 				});
 
 
