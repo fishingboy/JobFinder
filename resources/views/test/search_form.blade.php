851a32b1fa1c095733291fa44a7af996833fd5b7
Leo.Kuo <et282523@hotmail.com>
2018-04-22 20:26:00 +0800

    加上說明

--- resources/views/test/search_form.blade.php
+++ resources/views/test/search_form.blade.php
@@ -5,7 +5,7 @@
     @foreach($orderby as $key => $value)
         <input type='hidden' name='orderby[{{ $key }}]' value='{{ $value }}'>
     @endforeach
-    <b>包含:</b> <input type='text' name='keyword' value='{{ $keyword }}' style='width:300px;'>
-    <b>不包含</b> <input type='text' name='not_keyword' value='{{ $not_keyword }}' style='width:300px;'>
-    <input type='submit' value='搜尋'>
+    <div style="padding: 2px;"><b>包含(, 分隔，有其中一個關鍵字就有):</b> <input type='text' name='keyword' value='{{ $keyword }}' style='width:300px;'></div>
+    <div style="padding: 2px;"><b>不包含(, 分隔，不能出現任一個關鍵字):</b> <input type='text' name='not_keyword' value='{{ $not_keyword }}' style='width:300px;'> <input type='submit' value='搜尋'></div>
+    
 </form>
